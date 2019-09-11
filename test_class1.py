a0b9513406eff6da9e9dd74d5b6858e7dfcadf0c
ChrisStreadbeck <han0987@gmail.com>
2019-09-11 15:52:35 -0600

    spelling mistake

--- test_class1.py
+++ test_class1.py
@@ -5,19 +5,19 @@ class TestClass1(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         print("*#" * 30)
-        print("Class 2 -> class level setUp")
+        print("Class 1 -> class level setUp")
         print("*#" * 30)
 
     def test_class2_methodA(self):
-        print("Running class 2 -> method A")
+        print("Running class 1 -> method A")
 
     def test_class2_methodB(self):
-        print("Running class 2 -> method B")
+        print("Running class 1 -> method B")
 
     @classmethod
     def tearDownClass(self):
         print("*#" * 30)
-        print("Class 2 -> class level tearDown")
+        print("Class 1 -> class level tearDown")
         print("*#" * 30)
 
 if __name__ == '__main__':
